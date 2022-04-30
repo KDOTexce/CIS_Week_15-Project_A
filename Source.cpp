c80e557e967aeb97202f87348268f0154a345215
KurtK <cooldude@yahoo.com>
2022-04-30 19:10:34 -0400

    /* First commit */ Made the general template for main, no code yet.

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+/*
+* Kurt Kangas
+* CIS 1202 R02
+* 04/30/2022
+* Programming Assignment #15a
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+
+
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
