2ebda3ed27678b5a13c1d60978d41634653fb169
KurtK <cooldude@yahoo.com>
2022-04-30 20:38:05 -0400

    First Sample Output achieved. Program offsets char variable by 1.

--- Source.cpp
+++ Source.cpp
@@ -11,12 +11,36 @@
 
 using namespace std;
 
+char character(char start, int offset);
+
+
 int main()
 {
+	try
+	{
+		char ch = character('a', 1);
+		cout << ch << endl;
 
-
+	}
+	catch (string Exceptions)
+	{
+		cout << Exceptions;
+	}
 
 	cout << endl << endl;
 	system("pause");
 	return 0;
+}
+
+char character(char start, int offset)
+{
+	int startValue = int(start);
+	int target = startValue + offset;
+
+	if (start < 'A' || (start > 'Z' && start < 'a') || start > 'z')
+	{
+		string invalidCharacterException = "\nError: Invalid Character";
+		throw invalidCharacterException;
+	}
+	return start + offset;
 }
